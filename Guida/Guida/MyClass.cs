0398bf383aa51a2edf1c8ef70c1877babbf1a4e3
Sean Iamartino <siama001@fiu.edu>
2016-05-27 19:05:22 -0400

    test implementation getting sqlite working

--- Guida/Guida/MyClass.cs
+++ Guida/Guida/MyClass.cs
@@ -1,33 +1,58 @@
 ﻿using System;
+using SQLite;
+using System.IO;
 
 namespace Guida
 {
-    public class User
+    [Table("Doctors")]
+    public class Doctor
     {
-        public string username, password;
-        public User(String username,String password)
-        {
-            this.username = username;
-            this.password = password;
-        }
+      [PrimaryKey,Column("username")]
+      public String username { get; set; }
+      [Column("password")]
+      public String password { get; set; }
     }
 
 	public class MyClass
 	{
-        User user;
+        SQLiteConnection db;
         public MyClass ()
 		{
-            user = new User("Sean", "123");
-		}
+            var sqliteFilename = "MyDatabase.db3";
+#if __ANDROID__
+            // Just use whatever directory SpecialFolder.Personal returns
+            string libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+#else
+            // we need to put in /Library/ on iOS5.1 to meet Apple's iCloud terms
+            // (they don't want non-user-generated data in Documents)
+            string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal); // Documents folder
+            string libraryPath = Path.Combine (documentsPath, "..", "Library"); // Library folder instead
+#endif
+            var path = Path.Combine(libraryPath, sqliteFilename);
+            db = new SQLiteConnection(path);
+            Console.WriteLine("Creating database, if it doesn't already exist");
+            db.CreateTable<Doctor>();
+            if(db.Table<Doctor> ().Count() == 0)
+            {
+                var user = new Doctor();
+                user.username = "Sean";
+                user.password = "12345";
+                db.Insert(user);
+            }
+        }
 
         public bool authenticate(string username, string password)
         {
-            if(username.Equals(user.username, StringComparison.CurrentCultureIgnoreCase)  && password == user.password) {
-                return true;
-            }else
+            var users = db.Table<Doctor>();
+            bool auth = false;
+            foreach (var doc in users)
             {
-                return false;
+                if (doc.username == username && doc.password == password)
+                {
+                    auth = true;
+                }
             }
+            return auth;
         }
 	}
 }
