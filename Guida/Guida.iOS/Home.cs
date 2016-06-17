a925cadacd1b7aa2cacffb1c894889a237f9bce6
Alan Nieto <Alan@Alans-MacBook-Pro.local>
2016-06-16 23:39:50 -0400

    Added menu page on iOS front end

---
+++ Guida/Guida.iOS/Home.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using UIKit;
+
+namespace Guida.iOS
+{
+	public partial class Home : UIViewController
+	{
+		public Home(IntPtr handle) : base (handle)
+		{
+		}
+
+		public Home() : base("Home", null)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
--- Guida/Guida.iOS/ViewController.cs
+++ Guida/Guida.iOS/ViewController.cs
@@ -14,15 +14,40 @@ namespace Guida.iOS
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
-			Controller log = new Controller();
+			Controller mc = new Controller();
+			//DOCTORS
+			mc.addUser("Alan", "12345");
+			mc.addUser("Sean", "12345");
+			mc.addUser("Mohsen", "12345");
+			mc.addUser("Sadjadi", "12345");
+			mc.addUser("Giri", "12345");
+			mc.addUser("Trevor", "12345");
+
+			//PATIENTS
+			mc.addPatient("Sergio", "Jul");
+			mc.addPatient("Karla", "Aug");
+			mc.addPatient("Richard", "Jan");
+			mc.addPatient("Evelyn", "Dec");
+			mc.addPatient("Guillermo", "Oct");
+			mc.addPatient("Eduardo", "Sep");
+
+			//DOCTOR-PATIENT
+			mc.addDoctorPatient(1, "Alan");
+			mc.addDoctorPatient(2, "Alan");
+			mc.addDoctorPatient(3, "Alan");
+			mc.addDoctorPatient(4, "Sean");
+			mc.addDoctorPatient(5, "Giri");
+			mc.addDoctorPatient(6, "Trevor");
+
 			Login.TouchUpInside += (object sender, EventArgs e) => {
-				if (log.logIn(Username.Text,Password.Text)){
-					
+				if (mc.logIn(Username.Text,Password.Text)){
+
 					//Login.SetTitle("Logged in!",UIControlState.Normal);
-					UIViewController home = Storyboard.InstantiateViewController ("HomeViewController") as HomeViewController;
+					Username.Text = "";
+					Password.Text = "";
+					UIViewController home = Storyboard.InstantiateViewController ("Home") as Home;
 					this.NavigationController.PushViewController (home, true);
 				}
-
 			};
 
 
@@ -36,3 +61,7 @@ namespace Guida.iOS
 	}
 }
 
+/*
+UIViewController home = Storyboard.InstantiateViewController("HomeViewController") as HomeViewController;
+this.NavigationController.PushViewController (home, true);
+*/
