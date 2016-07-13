5325b9d4feff77f13f563e59b2b40b613d6c3873
Alan Nieto <Alan@Alans-MacBook-Pro.local>
2016-07-13 12:09:30 -0400

    background

--- Guida/Guida.Droid/Resources/Resource.designer.cs
+++ Guida/Guida.Droid/Resources/Resource.designer.cs
@@ -78,8 +78,8 @@ namespace Guida.Droid
 			// aapt resource value: 0x7f050001
 			public const int antibioticNameText = 2131034113;
 			
-			// aapt resource value: 0x7f050007
-			public const int antibioticPrescriptionButton = 2131034119;
+			// aapt resource value: 0x7f050008
+			public const int antibioticPrescriptionButton = 2131034120;
 			
 			// aapt resource value: 0x7f050006
 			public const int antibioticSearchButton = 2131034118;
@@ -87,44 +87,47 @@ namespace Guida.Droid
 			// aapt resource value: 0x7f050005
 			public const int antibitoicSearchField = 2131034117;
 			
-			// aapt resource value: 0x7f050010
-			public const int authStatusText = 2131034128;
+			// aapt resource value: 0x7f050011
+			public const int authStatusText = 2131034129;
+			
+			// aapt resource value: 0x7f050013
+			public const int displayPatientInfo = 2131034131;
 			
 			// aapt resource value: 0x7f050012
-			public const int displayPatientInfo = 2131034130;
-			
-			// aapt resource value: 0x7f050011
-			public const int displayPatientName = 2131034129;
-			
-			// aapt resource value: 0x7f05000a
-			public const int infoLabel = 2131034122;
-			
-			// aapt resource value: 0x7f05000c
-			public const int infoLabel2 = 2131034124;
+			public const int displayPatientName = 2131034130;
 			
 			// aapt resource value: 0x7f05000b
-			public const int listView1 = 2131034123;
+			public const int infoLabel = 2131034123;
 			
-			// aapt resource value: 0x7f05000f
-			public const int loginButton = 2131034127;
+			// aapt resource value: 0x7f05000d
+			public const int infoLabel2 = 2131034125;
+			
+			// aapt resource value: 0x7f05000c
+			public const int listView1 = 2131034124;
+			
+			// aapt resource value: 0x7f050010
+			public const int loginButton = 2131034128;
 			
 			// aapt resource value: 0x7f050004
 			public const int lungButton = 2131034116;
 			
-			// aapt resource value: 0x7f05000e
-			public const int passwordField = 2131034126;
-			
-			// aapt resource value: 0x7f050008
-			public const int patientInformationButton = 2131034120;
+			// aapt resource value: 0x7f05000f
+			public const int passwordField = 2131034127;
 			
 			// aapt resource value: 0x7f050009
-			public const int searchAntibioticButton = 2131034121;
+			public const int patientInformationButton = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int searchAntibioticButton = 2131034122;
+			
+			// aapt resource value: 0x7f050007
+			public const int tabWidget1 = 2131034119;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
 			
-			// aapt resource value: 0x7f05000d
-			public const int usernameField = 2131034125;
+			// aapt resource value: 0x7f05000e
+			public const int usernameField = 2131034126;
 			
 			static Id()
 			{
